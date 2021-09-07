da75f201ae6bd1cf370fa04ef7ccdbfc5c778244
Steele Farnsworth <swfarnsworth@gmail.com>
2021-09-07 07:30:09 -0400

    Add `<3.9` to `python_requires`

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
               "License :: OSI Approved :: Apache Software License",
               "Operating System :: OS Independent"
           ],
-          python_requires='>=3.7',
+          python_requires='>=3.7, <3.9',
           setup_requires=setup_requires,
           install_requires=install_requires,
           packages=find_packages())
